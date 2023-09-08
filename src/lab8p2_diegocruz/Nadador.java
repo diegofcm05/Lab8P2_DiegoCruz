e80e28d76a565f2dd2f0c1d06b4c17179276cc55
Diego Cruz <dfcm950@gmail.com>
2023-09-08 13:45:28 -0600

    Creacion clase Nadador

--- src/lab8p2_diegocruz/Nadador.java
+++ src/lab8p2_diegocruz/Nadador.java
@@ -10,4 +10,99 @@ package lab8p2_diegocruz;
  */
 public class Nadador {
     
+    private String nombre;
+    private Pais nacionalidad;
+    private int edad;
+    private double estatura, tiempo;
+    private String estilo;
+    private int distancia, medallas;
+
+    public Nadador(String nombre, Pais nacionalidad, int edad, double estatura, double tiempo, String estilo, int distancia, int medallas) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.edad = edad;
+        this.estatura = estatura;
+        this.tiempo = tiempo;
+        this.estilo = estilo;
+        this.distancia = distancia;
+        this.medallas = medallas;
+    }
+
+    public Nadador() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(Pais nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public int getMedallas() {
+        return medallas;
+    }
+
+    public void setMedallas(int medallas) {
+        this.medallas = medallas;
+    }
+
+    @Override
+    public String toString() {
+        return nombre+"-"+nacionalidad.getNombre();
+    }
+    
+    
+    
+    
+    
+    
 }
