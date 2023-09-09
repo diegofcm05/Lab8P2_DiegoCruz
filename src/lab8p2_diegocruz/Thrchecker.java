c2d8cb11c584e0b23eafeaf9c31524004c0c89ce
Diego Cruz <dfcm950@gmail.com>
2023-09-08 19:20:35 -0600

    Simulacion Lista

--- src/lab8p2_diegocruz/Thrchecker.java
+++ src/lab8p2_diegocruz/Thrchecker.java
@@ -16,14 +16,36 @@ public class Thrchecker extends Thread{
     private JProgressBar pbnad1;
     private JProgressBar pbnad2;
     private JProgressBar pbnad3;
+    private ThrSim x;
+    private ThrSim y;
+    private ThrSim z;
+    private String est;
+    private int dist;
+    private double rec;
+    private Nadador a;
+    private Nadador b;
+    private Nadador c;
+    
     private boolean avanzar;
     private boolean vive;
     private boolean acabo;
     
-    public Thrchecker(JProgressBar pbnad1, JProgressBar pbnad2, JProgressBar pbnad3) {
+    public Thrchecker(JProgressBar pbnad1, JProgressBar pbnad2, JProgressBar pbnad3, ThrSim x, ThrSim y, ThrSim z, String est, int dist, double rec, Nadador a, Nadador b, Nadador c) {
         this.pbnad1 = pbnad1;
         this.pbnad2 = pbnad2;
         this.pbnad3 = pbnad3;
+        this.x =x;
+        this.y =y;
+        this.z =z;
+        this.est = est;
+        this.dist = dist;
+        this.rec = rec;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        
+        
+        
         avanzar = true;
         vive = true;
     }
@@ -84,7 +106,25 @@ public class Thrchecker extends Thread{
         while (vive) {
             if ( pbnad1.getValue() == 100 || pbnad2.getValue() == 100 || pbnad3.getValue() == 100){
                 acabo = true;
+                
+                x.stop();
+                y.stop();
+                z.stop();
+                
                 JOptionPane.showMessageDialog(null,"Tenemos un ganador!");
+                int valor = Math.max(pbnad1.getValue(), pbnad2.getValue());
+                valor = Math.max(valor, pbnad2.getValue());
+                if (valor == pbnad1.getValue()){
+                    JOptionPane.showMessageDialog(null, "Evento:\nEstilo:"+est+"\nDistancia:"+dist+"\nRecord"+rec+"\nGanador: "+a.getNombre());
+                }
+                else if (valor == pbnad2.getValue()){
+                    JOptionPane.showMessageDialog(null, "Evento:\nEstilo:"+est+"\nDistancia:"+dist+"\nRecord"+rec+"\nGanador: "+b.getNombre());
+                }
+                else if (valor == pbnad3.getValue()){
+                    JOptionPane.showMessageDialog(null, "Evento:\nEstilo:"+est+"\nDistancia:"+dist+"\nRecord"+rec+"\nGanador: "+c.getNombre());
+                    
+                }
+                
                 vive = false;
             }
             try {
